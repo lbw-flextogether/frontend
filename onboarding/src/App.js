07918d76e358d30dc70c21e08d015f74655776e5
Nedu Robert <nedumrobert@gmail.com>
2019-03-13 22:51:50 +0100

    Feat: App component styling

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -8,16 +8,17 @@ import Info from './components/Info';
 import Time from './components/Time';
 import BuddyInfo from './components/Buddy/BuddyInfo';
 import Complete from './components/Complete';
-import BuddyVerify from './components/Buddy/BuddyVerify'
-import BuddyConfirm from './components/Buddy/BuddyConfirm'
-import BuddyComplete from './components/Buddy/BuddyComplete'
-import Manual from './components/Manual'
+import BuddyVerify from './components/Buddy/BuddyVerify';
+import BuddyConfirm from './components/Buddy/BuddyConfirm';
+import BuddyComplete from './components/Buddy/BuddyComplete';
+import Manual from './components/Manual';
+import './assets/styles/App.css'
 
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <>
+        <div className="appContainer">
         <Route exact path="/" component={Welcome} />
         <Route path="/start" component={Start} />
         <Route path="/info" component={Info} />
@@ -28,7 +29,7 @@ class App extends React.Component {
         <Route path="/confirm" component={BuddyConfirm} />
         <Route path="/buddycomplete" component={BuddyComplete} />
         <Route path="/manual" component={Manual} />
-        </>
+        </div>
       </Router>
     );
   }
