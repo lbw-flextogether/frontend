782412368415dfffdaca93b88b4e8c5e0e3efcec
Kevin Smith <keveightysev@gmail.com>
2019-12-02 08:27:49 -0600

    Removed slider test

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,44 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import Start from './components/Start';
-import Info from './components/Info';
-import Time from './components/Time';
-import BuddyInfo from './components/Buddy/BuddyInfo';
-import Complete from './components/Complete';
-import BuddyVerify from './components/Buddy/BuddyVerify';
-import BuddyConfirm from './components/Buddy/BuddyConfirm';
-import BuddyComplete from './components/Buddy/BuddyComplete';
-import Manual from './components/Manual';
-import EmailConfirm from './components/EmailConfirm'
-import SliderTest from './SliderTest';
-import './assets/styles/App.css'
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import Start from "./components/Start";
+import Info from "./components/Info";
+import Time from "./components/Time";
+import BuddyInfo from "./components/Buddy/BuddyInfo";
+import Complete from "./components/Complete";
+import BuddyVerify from "./components/Buddy/BuddyVerify";
+import BuddyConfirm from "./components/Buddy/BuddyConfirm";
+import BuddyComplete from "./components/Buddy/BuddyComplete";
+import Manual from "./components/Manual";
+import EmailConfirm from "./components/EmailConfirm";
+import "./assets/styles/App.css";
 
 class App extends React.Component {
   render() {
     return (
       <Router>
         <>
-        <Header />
-        <main>
-        <div className="appContainer">
-        <Route exact path="/" component={Welcome} />
-        <Route path="/start" component={Start} />
-        <Route path="/info" component={Info} />
-        <Route path="/time" component={Time} />
-        <Route path="/buddyinfo" component={BuddyInfo} />
-        <Route path="/complete" component={Complete} />
-        <Route exact path='/invite/:id' component={BuddyVerify} />
-        <Route path="/invite/:id/confirm" component={BuddyConfirm} />
-        <Route path="/invite/:id/buddycomplete" component={BuddyComplete} />
-        <Route path="/invite/:id/manual" component={Manual} />
-        <Route path='/verify/:id' component={EmailConfirm} />
-        <Route path='/SliderTest' component={SliderTest} />
-        </div>
-        </main>
+          <Header />
+          <main>
+            <div className="appContainer">
+              <Route exact path="/" component={Welcome} />
+              <Route path="/start" component={Start} />
+              <Route path="/info" component={Info} />
+              <Route path="/time" component={Time} />
+              <Route path="/buddyinfo" component={BuddyInfo} />
+              <Route path="/complete" component={Complete} />
+              <Route exact path="/invite/:id" component={BuddyVerify} />
+              <Route path="/invite/:id/confirm" component={BuddyConfirm} />
+              <Route
+                path="/invite/:id/buddycomplete"
+                component={BuddyComplete}
+              />
+              <Route path="/invite/:id/manual" component={Manual} />
+              <Route path="/verify/:id" component={EmailConfirm} />
+            </div>
+          </main>
         </>
       </Router>
     );
