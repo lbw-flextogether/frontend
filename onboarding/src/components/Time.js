04ec8787e72c7d1c3accf421eed3b886e6e865d4
Kevin Smith <keveightysev@gmail.com>
2020-01-22 16:31:40 -0600

    Converted to functional

--- onboarding/src/components/Time.js
+++ onboarding/src/components/Time.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Day from "./TimePicker/Day";
 import moment from "moment-timezone";
 import { connect } from "react-redux";
 import { addTimes } from "../actions";
 import Slider from "react-slick";
 
-class Time extends React.Component {
-  state = {
+const Time = ({ addTimes, history }) => {
+  const initState = {
     timezone: "America/Chicago",
     selectedTimes: {
       Sunday: [],
@@ -18,114 +18,110 @@ class Time extends React.Component {
       Saturday: []
     }
   };
+  const [state, setState] = useState(initState);
 
-  selectTime = (day, time, clicked) => {
+  const selectTime = (day, time, clicked) => {
     if (!clicked) {
-      this.setState({
+      setState({
+        ...state,
         selectedTimes: {
-          ...this.state.selectedTimes,
-          [day]: this.state.selectedTimes[day].filter(item => item !== time)
+          ...state.selectedTimes,
+          [day]: state.selectedTimes[day].filter(item => item !== time)
         }
       });
     } else {
-      this.setState({
+      setState({
+        ...state,
         selectedTimes: {
-          ...this.state.selectedTimes,
-          [day]: [...this.state.selectedTimes[day], time]
+          ...state.selectedTimes,
+          [day]: [...state.selectedTimes[day], time]
         }
       });
     }
   };
 
-  handleTimeZone = e => {
-    this.setState({
+  const handleTimeZone = e => {
+    setState({
+      ...state,
       timezone: e.target.value
     });
   };
 
-  handleNext = e => {
+  const handleNext = e => {
     e.preventDefault();
-    const time = Object.keys(this.state.selectedTimes)
-      .filter(day => this.state.selectedTimes[day].length > 0)
+    const time = Object.keys(state.selectedTimes)
+      .filter(day => state.selectedTimes[day].length > 0)
       .map(day => ({
         day: day,
-        timeslots: this.state.selectedTimes[day]
+        timeslots: state.selectedTimes[day]
       }));
-    this.props.addTimes(time, this.state.timezone);
-    this.props.history.push("/buddyinfo");
+    addTimes(time, state.timezone);
+    history.push("/buddyinfo");
   };
-
-  render() {
-    const settings = {
-      infinite: true,
-      speed: 500,
-      slidesToShow: 7,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 7,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 890,
-          settings: {
-            slidesToShow: 5,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 590,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 430,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 7,
+          slidesToScroll: 1
         }
-      ]
-    };
-    return (
-      <section className="time">
-        <h2>What's a good time to complete the program?</h2>
-        <p>
-          To complete the beta, we are asking that people commit to one 30
-          minute time block once a week. Please choose what times work well for
-          you.
-        </p>
-        <label htmlFor="timezone">Select Your Time Zone:</label>
-        <select
-          value={this.state.timezone}
-          onChange={this.handleTimeZone}
-          id="timezone"
-        >
-          <option></option>
-          {moment.tz.names().map(name => (
-            <option key={name}>{name}</option>
+      },
+      {
+        breakpoint: 890,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 590,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 430,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <section className="time">
+      <h2>What's a good time to complete the program?</h2>
+      <p>
+        To complete the beta, we are asking that people commit to one 30 minute
+        time block once a week. Please choose what times work well for you.
+      </p>
+      <label htmlFor="timezone">Select Your Time Zone:</label>
+      <select value={state.timezone} onChange={handleTimeZone} id="timezone">
+        <option></option>
+        {moment.tz.names().map(name => (
+          <option key={name}>{name}</option>
+        ))}
+      </select>
+
+      <div className="days">
+        <Slider {...settings}>
+          {Object.keys(state.selectedTimes).map(day => (
+            <Day day={day} key={day} selectTime={selectTime} />
           ))}
-        </select>
+        </Slider>
+      </div>
 
-        <div className="days">
-          <Slider {...settings}>
-            {Object.keys(this.state.selectedTimes).map(day => (
-              <Day day={day} key={day} selectTime={this.selectTime} />
-            ))}
-          </Slider>
-        </div>
-
-        <button onClick={this.handleNext} className="next-btn">
-          Next
-        </button>
-      </section>
-    );
-  }
-}
+      <button onClick={handleNext} className="next-btn">
+        Next
+      </button>
+    </section>
+  );
+};
 
 export default connect(null, { addTimes })(Time);
