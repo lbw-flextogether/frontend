d7c1f6d523fd50951867c524b539ba25f3186e7a
Kevin Smith <keveightysev@gmail.com>
2020-01-22 16:39:06 -0600

    Converted to functional, added prop types

--- onboarding/src/components/TimePicker/Day.js
+++ onboarding/src/components/TimePicker/Day.js
@@ -1,62 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
+
 import Slot from "./Slot";
 import { generateTimeslots } from "./generateTimeslots";
 
 import "../../assets/styles/Day.css";
 
-class Day extends React.Component {
-  state = {
-    scroll: 0
+const Day = ({ day, selectTime }) => {
+  const [scroll, setScroll] = useState(0);
+  const time = useRef(null);
+
+  useEffect(() => {
+    time.current.scrollTop = scroll;
+  }, [scroll, time]);
+
+  const scrollDown = () => {
+    setScroll(scroll + 275);
   };
 
-  scrollDown = () => {
-    this.setState(
-      { scroll: this.state.scroll + 275 },
-      () => (this.refs.time.scrollTop = this.state.scroll)
-    );
+  const scrollUp = () => {
+    setScroll(scroll - 275);
   };
 
-  scrollUp = () => {
-    this.setState(
-      { scroll: this.state.scroll - 275 },
-      () => (this.refs.time.scrollTop = this.state.scroll)
-    );
-  };
+  const timeSlots = generateTimeslots(30, "06:00", "23:00");
 
-  render() {
-    const timeSlots = generateTimeslots(30, "06:00", "23:00");
-    return (
-      <div className="slick-slide">
-        <h3>{this.props.day}</h3>
-        <div className="time-slots-container">
-          <div className="time-slots" ref="time">
-            {timeSlots.map(slot => (
-              <Slot
-                day={this.props.day}
-                slot={slot}
-                key={slot}
-                selectTime={this.props.selectTime}
-              />
-            ))}
-          </div>
-          {this.state.scroll === 0 ? (
-            <div onClick={this.scrollDown} className="scroll">
-              Later times &#128899;
-            </div>
-          ) : this.state.scroll >= 500 ? (
-            <div onClick={this.scrollUp} className="scroll">
-              Earlier times &#128897;
-            </div>
-          ) : (
-            <div className="scroll">
-              <span onClick={this.scrollUp}>&#128897;</span>
-              <span onClick={this.scrollDown}> &#128899;</span>
-            </div>
-          )}
+  return (
+    <div className="slick-slide">
+      <h3>{day}</h3>
+      <div className="time-slots-container">
+        <div className="time-slots" ref={time}>
+          {timeSlots.map(slot => (
+            <Slot day={day} slot={slot} key={slot} selectTime={selectTime} />
+          ))}
         </div>
+        {scroll === 0 ? (
+          <div onClick={scrollDown} className="scroll">
+            Later times &#128899;
+          </div>
+        ) : scroll >= 500 ? (
+          <div onClick={scrollUp} className="scroll">
+            Earlier times &#128897;
+          </div>
+        ) : (
+          <div className="scroll">
+            <span onClick={scrollUp}>&#128897;</span>
+            <span onClick={scrollDown}> &#128899;</span>
+          </div>
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+Day.propTypes = {
+  day: PropTypes.string.isRequired,
+  selectTime: PropTypes.func.isRequired
+};
 
 export default Day;
