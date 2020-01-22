e92e529f986402ab1b37a36c09057d57a14edc42
Kevin Smith <keveightysev@gmail.com>
2020-01-22 15:25:03 -0600

    Refactored to functional, added prop types

--- onboarding/src/components/Start.js
+++ onboarding/src/components/Start.js
@@ -1,58 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
 import { addRole } from "../actions";
 
-class Start extends React.Component {
-  state = {
-    role: null
-  };
+const Start = ({ addRole, history }) => {
+  const [role, setRole] = useState(null);
 
-  handleRole = e => {
+  useEffect(() => {
+    addRole(role);
+  }, [role]);
+
+  const handleRole = e => {
     e.preventDefault();
     const bool = e.target.name === "companion" ? true : false;
-    this.setState(
-      {
-        role: bool
-      },
-      () => this.props.addRole(this.state.role)
-    );
+    setRole(bool);
   };
 
-  handleNext = e => {
+  const handleNext = e => {
     e.preventDefault();
-    this.props.history.push("/info");
+    history.push("/info");
   };
 
-  render() {
-    return (
-      <section className="start">
-        <h2>Who are you?</h2>
-        <p>Choose 1:</p>
-        <div>
-          <button
-            name="senior"
-            onClick={this.handleRole}
-            className={this.state.role === false ? "active" : null}
-          >
-            I am interested in low impact exercise
-          </button>
-          <button
-            name="companion"
-            onClick={this.handleRole}
-            className={this.state.role === true ? "active" : null}
-          >
-            I want to be a companion to my loved one/friend who needs to do low
-            impact exercise
-          </button>
-        </div>
-        {this.state.role !== null ? (
-          <button onClick={this.handleNext} className="next-btn">
-            Next
-          </button>
-        ) : null}
-      </section>
-    );
-  }
-}
+  return (
+    <section className="start">
+      <h2>Who are you?</h2>
+      <p>Choose 1:</p>
+      <div>
+        <button
+          name="senior"
+          onClick={handleRole}
+          className={role === false ? "active" : null}
+        >
+          I am interested in low impact exercise
+        </button>
+        <button
+          name="companion"
+          onClick={handleRole}
+          className={role === true ? "active" : null}
+        >
+          I want to be a companion to my loved one/friend who needs to do low
+          impact exercise
+        </button>
+      </div>
+      {role !== null ? (
+        <button onClick={handleNext} className="next-btn">
+          Next
+        </button>
+      ) : null}
+    </section>
+  );
+};
+
+Start.propTypes = {
+  addRole: PropTypes.func.isRequired,
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired
+};
 
 export default connect(null, { addRole })(Start);
